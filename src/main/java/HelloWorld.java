764dcd6a77f3843fd03ccf8ad73b27f9a8eba934
cadl <ctrlaltdeleteliu@gmail.com>
2022-12-12 15:04:54 +0800

    fix source/doc validation failed

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    /**
+     * @param args
+     */
+    // generate main function
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
